b63d9bcd7ed0c457dbe5c453cf9259d77488c0d2
lukasurmava <surmava0@gmail.com>
2022-08-29 20:05:35 +0400

    dummy

--- ToDoAPI/Models/TodoItem.cs
+++ ToDoAPI/Models/TodoItem.cs
@@ -4,6 +4,15 @@
     {
         public long Id { get; set; }
         public string? Name { get; set; }
+        public string? Description { get; set; }
+        public ActivityTypeEnum ActivityType { get; set; }
         public bool IsComplete { get; set; }
+    }
+
+    public enum ActivityTypeEnum
+    {
+        Productive = 0,
+        Relaxing = 1,
+        Wasting = 2
     }
 }
