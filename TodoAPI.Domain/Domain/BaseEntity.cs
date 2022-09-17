53d2c930d5d632fdd27021af47f5720c3d18a480
Petre <petrechitashvili@users.noreply.github.com>
2022-09-17 14:59:10 +0400

    Update BaseEntity.cs
    Use this instead https://github.com/vkhorikov/CSharpFunctionalExtensions/blob/master/CSharpFunctionalExtensions/Entity/Entity.cs
    
    @lukasurmava

--- TodoAPI.Domain/Domain/BaseEntity.cs
+++ TodoAPI.Domain/Domain/BaseEntity.cs
@@ -1,13 +1,19 @@
-﻿namespace TodoAPI.Domain.Domain
+﻿namespace TodoAPI.Domain.Domain;
+
+protected abstract class BaseEntity<TId>
 {
-    public abstract class BaseEntity
-    {
-        public long Id { get; set; }
+    public long TId { get; protected set; }
 
-        public DateTime CreateDate { get; set; }
+    public DateTime CreateDate { get; private set; }
 
-        public DateTime? DeleteDate { get; set; }
+    public DateTime? DeleteDate { get; private set; }
 
-        public DateTime? LastModifiedDate { get; set; }
-    }
+    public DateTime? LastModifiedDate { get; private set; }
 }
+
+protected abstract class BaseEntity : BaseEntity<long>
+{
+    
+}
+
+// Just use https://github.com/vkhorikov/CSharpFunctionalExtensions/blob/master/CSharpFunctionalExtensions/Entity/Entity.cs
